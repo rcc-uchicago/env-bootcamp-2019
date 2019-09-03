eb641c45f4febc5533ceed8894c75cfbbffd5983
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-09-03 16:20:54 -0500

    Revised the matrix Python demo.

--- Day2/matrixdemo.py
+++ Day2/matrixdemo.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+# SET UP ENVIRONMENT
+# ------------------
 # We use this short Python script to explore the computation involved
 # in an operation (multiplication) on a large matrix; this sort of
 # computation would be needed, for example, to estimate the p x p
@@ -8,13 +10,11 @@ import sys
 import time
 import numpy
 
+# SCRIPT PARAMETERS
+# -----------------
 # Set the matrix dimensions.
-if len (sys.argv) == 3:
-   n = int(sys.argv[1])
-   p = int(sys.argv[2])
-else:
-   n = 8000
-   p = 12000
+n = int(sys.argv[1])
+p = int(sys.argv[2])
 
 # CREATE DATA
 # -----------
@@ -22,10 +22,14 @@ else:
 print('Creating %d x %d matrix, X.' % (n,p))
 X = numpy.random.random_sample((n,p))
 
-# MATRIX COMPUTATION
-# ------------------
+# COMPUTE RESULTS
+# ---------------
 # Compute X'*X.
 print('Computing X\'*X (transpose of X times X).')
 t = time.time()
 R = X.T @ X
 print('Computation took %0.2f seconds.' % (time.time() - t))
+
+# SESSION INFO
+# ------------
+# TO DO.
